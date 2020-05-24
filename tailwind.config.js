8d2895d8584289131f6e3895d28a414b363070d8
Colin Dismuke <colin.dismuke@gmail.com>
2020-05-24 15:58:04 +0800

    :lipstick: Added dark mode and began implementing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
       backgroundOpacity: {
         "98": "0.98",
       },
+      screens: {
+        light: { raw: "(prefers-color-scheme: light)" },
+        dark: { raw: "(prefers-color-scheme: dark)" },
+      },
     },
   },
+  plugins: [
+    function ({ addBase, config }) {
+      addBase({
+        body: {
+          color: config("theme.colors.black"),
+          backgroundColor: config("theme.colors.white"),
+        },
+        "@screen dark": {
+          body: {
+            color: config("theme.colors.white"),
+            backgroundColor: config("theme.colors.black"),
+          },
+        },
+      });
+    },
+  ],
 };
