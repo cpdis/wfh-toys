4ff47142a96df7f6313eaad472bf4dcd9abebee4
Colin Dismuke <colin.dismuke@gmail.com>
2020-05-24 14:18:24 +0800

    :heavy_plus_sign: Added cssnano and purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+  ],
 };
